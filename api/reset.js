663b5bcfec093e115e99e6bf228dfc6e1bbb7335
jenildobariya999-art <jenildobariya999@gmail.com>
2026-06-07 12:36:47 +0530

    Update reset.js

--- api/reset.js
+++ api/reset.js
@@ -1,36 +1,21 @@
 import { Redis } from "@upstash/redis";
 
 const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_KV_REST_API_URL,
-  token: process.env.UPSTASH_REDIS_REST_KV_REST_API_TOKEN,
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN,
 });
 
 export default async function handler(req, res) {
 
-  if (req.method !== "POST") {
-    return res.status(405).json({
-      status: "fail"
-    });
-  }
-
-  const { fp, botusername } = req.body || {};
-
-  if (!fp || !botusername) {
-    return res.json({
-      status: "fail",
-      message: "Missing Data"
-    });
-  }
+  const { fp, botusername } = req.body;
 
   const key =
     `BOT_${botusername}:FP_${fp}`;
 
-  await redis.del(
-  `BOT_${botusername}:FP_${fp}`
-);
+  await redis.del(key);
 
   return res.json({
-    status: "pass",
-    message: "Reset Successful"
+    success: true
   });
+
 }
